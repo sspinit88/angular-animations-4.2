83890f21a280506e5551595dd8ff15ef7ed342ab
sspinit88 <sspinit88@gmail.com>
2019-07-10 20:36:04 +0300

    tesd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { animate, query, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,35 @@ import { animate, query, style, transition, trigger } from '@angular/animations'
   animations: [
     trigger('panelState', [
       transition(':enter', [
-        // todo использование  query (назначение анимации на конкретный элемент)
-        query('.panel-heading', [
-          style({
-            transform: 'translateY(-300px)',
-            opacity: 0,
-          }),
-          animate(500, )
+        group([
+          query(':self', [
+            style({
+              opacity: 0,
+            }),
+            animate(300)
+          ]),
+          // todo использование  query (назначение анимации на конкретный элемент)
+          query('.panel-heading', [
+            style({
+              transform: 'translateY(-300px)',
+              opacity: 0,
+            }),
+            animate(300)
+          ]),
+          query('.panel-body', [
+            style({
+              transform: 'translateX(-100%)',
+              opacity: 0,
+            }),
+            animate(300),
+          ]),
+          query('.panel-footer', [
+            style({
+              transform: 'translateY(100%)',
+              opacity: 0,
+            }),
+            animate(300),
+          ])
         ]),
       ]),
       transition(':leave', [
@@ -22,6 +44,23 @@ import { animate, query, style, transition, trigger } from '@angular/animations'
           transform: 'translateX(-100%)',
           opacity: 0
         }))
+      ]),
+      transition('* => *', [
+        query(':enter', [
+          style({
+            transform: 'scale(1)'
+          }),
+          animate(200, style({
+            transform: 'scale(1.1)'
+          })),
+          animate(100)
+        ], {optional: true}),
+        query('div p, button', [
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(200)
+        ])
       ])
     ])
   ]
@@ -29,4 +68,18 @@ import { animate, query, style, transition, trigger } from '@angular/animations'
 export class AppComponent {
   showPanel = false;
   showParagraph = true;
+
+/*
+    :enter /:leave  => Запросить добавленные / удаленные элементы
+
+    :animating  => Запросить все элементы, которые в данный момент воспроизводят анимацию
+
+    @triggerName  => Запросить все элементы, которые используют этот анимационный триггер
+
+    @*  => Запросить все элементы, которые имеют ЛЮБОЙ  анимационный триггер
+
+    :self  => Запросить родительский элемент (то есть тот, к которому был применен триггер, инициирующий переход)
+*/
 }
+
+
